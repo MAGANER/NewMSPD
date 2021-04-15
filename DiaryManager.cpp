eacce6d56496bd0990900e6342509cac69221f18
MAGANER <maganer.krik@mail.ru>
2021-04-15 22:30:51 +0300

    added functions to obtain entered topic, also added function(but not finished it) to save entered pages

--- DiaryManager.cpp
+++ DiaryManager.cpp
@@ -28,14 +28,42 @@ void DiaryManager::inner::read_pages_callback(Fl_Widget* button_ptr, void* pages
 }
 void DiaryManager::inner::add_page_callback(Fl_Widget* button_ptr, void* pages)
 {
-	Fl_Window* window      = new Fl_Window(720, 640, "write new page");
-	Fl_Text_Buffer* buff   = new Fl_Text_Buffer();
+	Fl_Window* window      = new Fl_Window(720, 650, "write new page");
+	buff   = new Fl_Text_Buffer();
 	Fl_Text_Editor* editor = new Fl_Text_Editor(20, 20, 600, 600, "Write all you think");
 	editor->buffer(buff);
 	window->resizable(*editor);
 
 	Fl_Button* save_button = new Fl_Button(640, 40, 70, 20, "save");
+	save_button->callback(save_page_callback, (void*)&pages);
+	save_button->when(FL_WHEN_RELEASE | FL_WHEN_ENTER_KEY);
+
+	Fl_Input* topic_input  = new Fl_Input(60, 620, 300, 20, "topic:");
+	topic_input->callback((Fl_Callback*)get_topic_callback);
+	topic_input->when(FL_WHEN_RELEASE | FL_WHEN_ENTER_KEY);
 
 	window->end();
 	window->show();
+}
+void DiaryManager::inner::get_topic_callback(Fl_Input* input)
+{
+	entered_topic = input->value();
+}
+void DiaryManager::inner::save_page_callback(Fl_Widget* button_ptr, void* pages)
+{
+	if (!entered_topic.empty())
+	{
+		//make page
+		string text = buff->text();
+
+		time_t now;
+		time(&now);
+		string current_time = ctime(&now);
+
+		DiaryPage* page = new DiaryPage(text, entered_topic, current_time);
+
+		//encrypt it
+		//add page to all pages
+		//rewrite diary file
+	}
 }
--- DiaryManager.h
+++ DiaryManager.h
@@ -1,13 +1,18 @@
 #pragma once
+#define _CRT_SECURE_NO_WARNINGS
 #include"Common.h"
 #include "FL/Fl.H"
 #include"FL/Fl_Button.H"
 #include"FL/Fl_Window.H"
 #include"FL/Fl_Text_Editor.H"
 #include"FL/Fl_Text_Buffer.H"
+#include"FL/Fl_Input.H"
 #include"Diary.h"
 #include<vector>
 #include<fstream>
+#include<string>
+#include<ctime>
+#include<iostream>
 
 namespace DiaryManager
 {
@@ -15,10 +20,16 @@ namespace DiaryManager
 
 	static Fl_Window* window = nullptr;
 	static vector<DiaryPage*> pages;
+
+	static Fl_Text_Buffer* buff = nullptr;
+	static string entered_topic;
+
 	namespace inner
 	{
 		static void read_diary(const string& path);
 		static void read_pages_callback(Fl_Widget* button_ptr,void* pages);
 		static void add_page_callback(Fl_Widget* button_ptr,void* pages);
+		static void save_page_callback(Fl_Widget* button_ptr, void* pages);
+		static void get_topic_callback(Fl_Input* input);
 	};
 };
