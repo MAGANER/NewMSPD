bca69a31eec07e099742d24a17e34b6bb9bc7dab
MAGANER <maganer.krik@mail.ru>
2021-04-16 19:04:43 +0300

    wrote functions to encrypt/decrypt data

--- Encryption.cpp
+++ Encryption.cpp
@@ -42,3 +42,24 @@ Encryption::SecByteBlock Encryption::convert_to_bytes(const string& key)
 	SecByteBlock _key(reinterpret_cast<CryptoPP::byte*>(&bytes[0]), AES::DEFAULT_KEYLENGTH);
 	return _key;
 }
+
+std::string Encryption::encrypt(const key_iv& key_iv, const string& text)
+{
+	CFB_Mode<AES>::Encryption encryptor(key_iv.first, AES::DEFAULT_KEYLENGTH, key_iv.second);
+
+	CryptoPP::byte* data = (CryptoPP::byte*)text.c_str();
+	size_t data_len = strlen((const char*)data) + 1;
+
+	encryptor.ProcessData(data, data, data_len);
+	return string((const char*)data);
+}
+std::string Encryption::decrypt(const key_iv& key_iv, const string& text)
+{
+	CFB_Mode<AES>::Decryption decryptor(key_iv.first, AES::DEFAULT_KEYLENGTH, key_iv.second);
+
+	CryptoPP::byte* data = (CryptoPP::byte*)text.c_str();
+	size_t data_len = strlen((const char*)data) + 1;
+
+	decryptor.ProcessData(data, data, data_len);
+	return string((const char*)data);
+}
--- Encryption.h
+++ Encryption.h
@@ -1,6 +1,7 @@
 #pragma once
 #include"rijndael.h"
 #include"osrng.h"
+#include"modes.h"
 #include<utility>
 #include<string>
 
@@ -13,4 +14,7 @@ namespace Encryption
 	key_iv get_random_key();
 	string convert_bytes(const SecByteBlock& bytes);
 	SecByteBlock convert_to_bytes(const string& key);
+
+	string encrypt(const key_iv& key_iv, const string& text);
+	string decrypt(const key_iv& key_iv, const string& text);
 };
